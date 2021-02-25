c14e0e82db59c157d535c9b5daa0905d5301b3b1
Andy Iverson <andy@andyiverson.me>
2021-02-25 17:21:23 -0600

    Show effective HP in unit viewer

--- src/components/UnitCard_Stats.svelte
+++ src/components/UnitCard_Stats.svelte
@@ -1,29 +1,28 @@
 <script>
-	import { STAT_NAMES, STAT_DISPLAY_NAMES, calculatePower } from "@src/priconne.js";
+	import { STAT_NAMES, STAT_DISPLAY_NAMES, calculatePower, 
+		calculateEffectivePhysicalHp, calculateEffectiveMagicHp } from "@src/priconne.js";
 	export let actor;
 
 	$: power = calculatePower(actor);
+	$: effectivePhysicalHp = calculateEffectivePhysicalHp(actor);
+	$: effectiveMagicHp = calculateEffectiveMagicHp(actor);
 </script>
 
 <div class="card-section">
 	<div class="card-section-header">Stats</div>
-	<div class="stat-power">
-		Power: {Math.round(power)}
-	</div>
 	<table>
+		<tr><td class="stat-label">Power</td><td class="stat-value">{Math.round(power)}</td></tr>
 		{#each STAT_NAMES as stat}
 		<tr><td class="stat-label">{STAT_DISPLAY_NAMES[stat]}</td><td class="stat-value">{Math.round(actor[stat])}</td></tr>
 		{/each}
+		<tr><td class="stat-label">Effective Physical HP</td><td class="stat-value">{Math.round(effectivePhysicalHp)}</td></tr>
+		<tr><td class="stat-label">Effective Magic HP</td><td class="stat-value">{Math.round(effectiveMagicHp)}</td></tr>
 	</table>
 </div>
 
 <style>
 div.card-section {
 	min-width: 210px;
-}
-
-div.stat-power {
-	margin-bottom: 8px;
 }
 
 td {
