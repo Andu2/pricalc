f8a2a2b28024c9c2b540af7be295311051164960
Andy Iverson <andy@andyiverson.me>
2021-02-25 11:10:28 -0600

    Change wording

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Paste master.db from datamine to src/data/master.db and run `npm run dump-db`
 ## Philosophy and Goals
 
  * *Enable enjoyment* - PriCalc is intended to be a tool that enables players to do their own analysis. I find overanalyzing games to be fun, and I'm hoping this tool will allow people to have fun in the same way.
- * *Desktop-first* - Although many players of a phone game are likely to visit this app on their phone, the tools will be designed on a desktop screen size first, and then made to fit in a mobile UI later. I find that mobile UIs are often overly simplistic, and I don't ever want to feel like I can't display all the data I want to because I can't fit it in the UI. That's not to say the mobile UI isn't important - it should be usable, but power users should be able to use a rich desktop UI.
+ 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,16 +13,15 @@
 {@html config.changeLog[0].notes.join("<br />")}
 </p>
 <hr />
-<h2>About</h2>
-<p>PriCalc was created by Andu.</p>
-<p><a href="https://github.com/Andu2/pricalc">Source code</a></p>
 <h2>Bug reports and ideas</h2>
-<p>If you notice an issue, or have a good idea for an analysis tool or guide, your input is very welcome! You may message me on reddit: u/AnduCrandu or on Discord: Andu#4916. Bugs may also be reported on <a href="https://github.com/Andu2/pricalc/issues">GitHub</a>.</p>
+<p>If you notice an issue, have a good idea for an analysis tool, or just want to talk, your input is very welcome! You may message me on reddit: u/AnduCrandu or on Discord: Andu#4916. Bugs may also be reported on <a href="https://github.com/Andu2/pricalc/issues">GitHub</a>.</p>
 <h2>Other Useful Tools</h2>
 <ul>
 	<li><a href="https://expugn.github.io/priconne-quest-helper/">Priconne Quest Helper</a> by S'pugn - Item farming calculator</li>
 	<li><a href="https://shioris-library.com/">Shiori's Library</a> by Ruiqi Mao - Unit and equipment database, includes an equipment farming calculator</li>
 	<li><a href="https://github.com/Dimbreath/PriconneData">PriconneData</a> by Dimbreath - A repository containing dumps of the game's database</li>
-	<li><a href="https://docs.google.com/spreadsheets/d/1JjK7Ws4gfzKChRs5ueoxEZVN5SXK10nhDC1-nbm0NUs/edit?usp=sharing">This spreadsheet</a> by ??? contains a bunch of stuff including formulas</li>
+	<li><a href="https://docs.google.com/spreadsheets/d/1JjK7Ws4gfzKChRs5ueoxEZVN5SXK10nhDC1-nbm0NUs/edit?usp=sharing">This spreadsheet</a> contains a bunch of stuff including formulas</li>
+	<li><a href="https://www.priconneglobal.info/">Priconne Global Info</a> by YoYoPop is an informational site focused on the Global server</li>
 	<li>Massive shoutout to esterTion for making the original datamining tools and S'pugn for making an <a href="https://github.com/Expugn/priconne-en_db-fetch">English version</a></li>
 </ul>
+<p><a href="https://github.com/Andu2/pricalc">PriCalc Source code</a></p>
