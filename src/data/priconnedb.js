b9f2873557625fd46002d5f2ff998df73d10a832
Andy Iverson <andy@andyiverson.me>
2021-08-25 23:13:21 -0500

    Fix HP recovery boost story bonus - see S Kokkoro

--- src/data/priconnedb.js
+++ src/data/priconnedb.js
@@ -251,7 +251,8 @@ export const NUMBER_TO_STAT = {
 	9: "life_steal",
 	10: "wave_hp_recovery", // Thank you Loppolutte for pointing out what this stat is
 	11: "wave_energy_recovery",
-	14: "don't know" // TODO. Christina has it.
+	14: "don't know", // TODO. Christina has it.
+	15: "hp_recovery_rate"
 }
 
 // Slightly different numbers for skills because why not
